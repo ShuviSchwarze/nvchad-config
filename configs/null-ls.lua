64c1922986501e3b18743d8c8c59e9f6dd417c6f
shuvischwarze <shuvischwarze@gmail.com>
2023-08-26 10:36:50 -0400

    Make prettier default

--- configs/null-ls.lua
+++ configs/null-ls.lua
@@ -5,8 +5,8 @@ local b = null_ls.builtins
 local sources = {
 
   -- webdev stuff
-  b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
-  b.formatting.prettier.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+  -- b.formatting.deno_fmt, -- Disabling deno to use .prettierrc config files
+  b.formatting.prettier,
 
   -- Lua
   b.formatting.stylua,
