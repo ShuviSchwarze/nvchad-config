d7a354ee12d3bc088fb49c2a9640fa4ea3a69295
shuvischwarze <shuvischwarze@gmail.com>
2023-09-29 11:32:54 +0700

    change lightspeed to leap

--- plugins.lua
+++ plugins.lua
@@ -91,7 +91,10 @@ local plugins = {
     },
   },
   {
-    "ggandor/lightspeed.nvim",
+    "ggandor/leap.nvim",
+    config = function()
+      require("leap").add_default_mappings()
+    end,
     lazy = false,
   },
 
