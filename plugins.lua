f792a31a9f1d67eb1a8cabaeca19605a58b13cc9
ShuviSchwarze <shuvischwarze@gmail.com>
2023-09-29 12:10:42 +0700

    use flash

--- plugins.lua
+++ plugins.lua
@@ -54,11 +54,11 @@ local plugins = {
   },
   {
     "tpope/vim-surround",
-    lazy = false,
+    event = "VeryLazy",
   },
   {
     "tpope/vim-repeat",
-    lazy = false,
+    event = "VeryLazy",
   },
   {
     "windwp/nvim-ts-autotag",
@@ -91,11 +91,53 @@ local plugins = {
     },
   },
   {
-    "ggandor/leap.nvim",
-    config = function()
-      require("leap").add_default_mappings()
-    end,
-    lazy = false,
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    ---@type Flash.Config
+    opts = {},
+        -- stylua: ignore
+    keys = {
+      {
+        "s",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").jump()
+        end,
+        desc = "Flash",
+      },
+      {
+        "S",
+        mode = { "n", "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+        desc = "Flash Treesitter",
+      },
+      {
+        "r",
+        mode = "o",
+        function()
+          require("flash").remote()
+        end,
+        desc = "Remote Flash",
+      },
+      {
+        "R",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter_search()
+        end,
+        desc = "Treesitter Search",
+      },
+      {
+        "<c-s>",
+        mode = { "c" },
+        function()
+          require("flash").toggle()
+        end,
+        desc = "Toggle Flash Search",
+      },
+    },
   },
 
   -- To make a plugin not be loaded
