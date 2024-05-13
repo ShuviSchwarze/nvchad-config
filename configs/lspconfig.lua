a7a4c4ccd7c6cc396c983e9e23c9706847837fba
Hai Duong <anh.duong@zen8labs.com>
2024-05-13 18:02:10 +0700

    fix vue volar

--- configs/lspconfig.lua
+++ configs/lspconfig.lua
@@ -2,13 +2,14 @@ local on_attach = require("plugins.configs.lspconfig").on_attach
 local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require "lspconfig"
-local typescript_tools = require "typescript-tools"
+
+-- local typescript_tools = require "typescript-tools"
 
 -- if you just want default config for the servers then put them in a table
 local servers = {
   -- "html",
   "cssls",
-  "tsserver",
+  -- "tsserver",
   -- "volar",
 
   -- "clangd",
@@ -34,6 +35,11 @@ for _, lsp in ipairs(servers) do
 end
 
 -- If tsserver broke again, and I swear to god it will, bump nvm node version
+
+local vue_typescript_plugin = require("mason-registry").get_package("vue-language-server"):get_install_path()
+  .. "/node_modules/@vue/language-server"
+  .. "/node_modules/@vue/typescript-plugin"
+
 lspconfig.tsserver.setup {
   on_attach = on_attach,
   capabilities = capabilities,
@@ -44,8 +50,9 @@ lspconfig.tsserver.setup {
     plugins = {
       {
         name = "@vue/typescript-plugin",
-        location = "anything",
+        location = vue_typescript_plugin,
         languages = {
+          "javascript",
           "typescript",
           "vue",
         },
@@ -54,14 +61,18 @@ lspconfig.tsserver.setup {
   },
   filetypes = {
     "javascript",
+    "javascriptreact",
+    "javascript.jsx",
     "typescript",
+    "typescriptreact",
+    "typescript.tsx",
     "vue",
   },
 }
 
-lspconfig.quick_lint_js.setup {
-  filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-}
+-- lspconfig.quick_lint_js.setup {
+--   filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+-- }
 
 -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 --   silent = true,
